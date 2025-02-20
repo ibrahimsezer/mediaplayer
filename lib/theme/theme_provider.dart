1114b9bfbc4d07e2aeb5b27d2ccecf1c6ca72659
İbrahim SEZER <ibrahimszr16@gmail.com>
2025-02-20 21:02:58 +0300

    Added App Theme Provider

---
+++ lib/theme/theme_provider.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:mediaplayer/theme/app_theme.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  bool _isDarkMode = false;
+
+  bool get isDarkMode => _isDarkMode;
+  ThemeData get currentTheme => _isDarkMode ? AppTheme.darkTheme : AppTheme.lightTheme;
+
+  void toggleTheme() {
+    _isDarkMode = !_isDarkMode;
+    notifyListeners();
+  }
+}
