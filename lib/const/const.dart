598f4410e5dab3023cce918cc918f04d15eb367d
İbrahim SEZER <ibrahimszr16@gmail.com>
2025-03-28 08:27:01 +0300

    Remove unused assets and codes

--- lib/const/const.dart
+++
@@ -1,8 +0,0 @@
-class ConstTexts {
-  static const String jswMediaPlayer = "JSW Media Player";
-}
-
-class AssetNames {
-  static const String defaultMusicLogo =
-      'lib/assets/images/default_music_photo.png';
-}
